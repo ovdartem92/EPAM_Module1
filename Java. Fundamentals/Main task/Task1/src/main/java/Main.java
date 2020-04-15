5edfbce176fd1dfde494faf8fd57fd6fecd0a619
Artem Ovdienko <ovd.artem92@gmail.com>
2020-04-15 11:45:11 +0300

    Task2

--- Java. Fundamentals/Main task/Task1/src/main/java/Main.java
+++ Java. Fundamentals/Main task/Task1/src/main/java/Main.java
@@ -1,10 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         System.out.println("Введите ваше имя и нажмите <Enter>:");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String s = reader.readLine();
